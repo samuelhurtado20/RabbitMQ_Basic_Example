cffb1f9aacdb5837a30424368d8342fdb544f79b
samuelhurtado20 <samuelhurtado20@hotmail.com>
2021-10-04 22:24:59 -0500

    example: basic queue

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        static void Main(string[] args)
+        {
+            string queueName = @"queue1";
+            string hostName = @"localhost";
+
+            var factory = new ConnectionFactory() { HostName = hostName };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                string message = @$"Hello World from RabbitMQ... ({DateTime.Now})";
+
+                channel.BasicPublish(exchange: "",
+                                    routingKey: @"queue1",
+                                    basicProperties: null,
+                                    body: Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($" [>>>] Sent {message}");
+            }
+
+            Console.WriteLine(" Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Subscriber/Subscriber.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Subscriber
+{
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            string queueName = @"queue1";
+            string hostName = @"localhost";
+
+            var factory = new ConnectionFactory() { HostName = hostName };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    Console.WriteLine($" [>>>] Msg Received: {message}");
+                    Console.WriteLine($"[>>>>>>>>>>>>>>>>>>>>>>>>>>>]");
+                };
+                channel.BasicConsume(queue: queueName,
+                                    autoAck: true,
+                                    consumer: consumer);
+
+                Console.WriteLine(@" Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
